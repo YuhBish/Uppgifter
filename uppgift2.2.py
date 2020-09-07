7a96a04a2d1011805f5954cd39ea64b25fc332e6
YuhBish <70145942+YuhBish@users.noreply.github.com>
2020-09-07 13:38:40 +0200

    uppgft

---
+++ uppgift2.2.py
@@ -0,0 +1,11 @@
+svar=input("Tal skriv nu! ")
+a=int(svar)
+b=a*a
+print ("Sicc tal bror", b)
+
+svar1=input("Ett till ")
+c=float(svar1)
+svar2=input("Du kan fler! ")
+d=float(svar2)
+e=c*d
+print(e)
