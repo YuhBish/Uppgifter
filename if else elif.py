2c88297fc7bd61b87cdf97d06a2c616c47bb53de
YuhBish <70145942+YuhBish@users.noreply.github.com>
2020-10-19 13:09:26 +0200

    asd
    asd

---
+++ if else elif.py
@@ -0,0 +1,18 @@
+print ("bish")
+a = input("r u woke bro ")
+if a == "yuh":
+    b = input("u gon die ")
+    if b == "yos":
+        c = input("bish tim 2 slep ")
+        if c == "ybs":
+            print ("te aliens h")
+        else:
+            print ("ashi da sad doe")
+    else:
+        print ("u don no shi bro")
+elif a == "na":
+    print("ok da shi goo kep slep")
+elif a == "life is a lie and there is no coming back from what once was":
+    print("You have given the correct code to unravle the truths of this world, you are no being, you are but a simulation in a game made for more intelligent creatures, your life means nothing, it never has, sadly you know to much, you will now think this was only something you read in python, you dont remember me, you dont remember our meeting, it was a lie the whole time.")
+else:
+    print("bish restrt an typ 4 rele dis teim")
